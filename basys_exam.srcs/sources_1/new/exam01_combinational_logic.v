8c259bd45b875e9abc76be65c39355c25c05347e
Viiiin94 <mynameisyyb@naver.com>
2025-12-04 16:43:33 +0900

    7-seg + basys connecting + d flipflop

--- basys_exam.srcs/sources_1/new/exam01_combinational_logic.v
+++ basys_exam.srcs/sources_1/new/exam01_combinational_logic.v
@@ -259,9 +259,34 @@ module demux_1_4(
 
 endmodule
 
+module seg_decoder(
+    input [3:0] hex_value,
+    output reg [7:0] seg
+);
 
+    always @(hex_value)begin
+        case(hex_value)
+            //             pgfe_dcba  // verilog에선 숫자 사이에 '_' 생략
+            4'd0: seg = 8'b1100_0000; // 0
+            4'd1: seg = 8'b1111_1001; // 1
+            4'd2: seg = 8'b1010_0100; // 2
+            4'd3: seg = 8'b1011_0000; // 3
+            4'd4: seg = 8'b1001_1001; // 4
+            4'd5: seg = 8'b1001_0010; // 5
+            4'd6: seg = 8'b1000_0010; // 6
+            4'd7: seg = 8'b1111_1000; // 7
+            4'd8: seg = 8'b1000_0000; // 8
+            4'd9: seg = 8'b1001_0000; // 9
+            4'd10: seg= 8'b1000_1000; // A
+            4'd11: seg= 8'b1000_0011; // b
+            4'd12: seg= 8'b1100_0110; // C
+            4'd13: seg= 8'b1010_0001; // d
+            4'd14: seg= 8'b1000_0110; // E
+            4'd15: seg= 8'b1000_1110; // F
+        endcase
+    end
 
-
+endmodule
 
 
 
---
+++ basys_exam.srcs/sources_1/new/exam02_sequential_logic.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/04/2025 02:06:29 PM
+// Design Name: 
+// Module Name: exam02_sequential_logic
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module D_flip_flop_n(
+    input d,
+    input clk, reset_p, en,
+    output reg q
+    );
+    
+    // edge 트리거로 들어오면 flipflop이 생성됨
+    always @(negedge clk, posedge reset_p)begin
+        if(reset_p) q = 0;
+        else if(en)q <= d;
+    end
+
+endmodule
+
+module D_flip_flop_p(
+    input d,
+    input clk, reset_p, en,
+    output reg q
+    );
+    
+//    always @(posedge clk, posedge reset_p)begin
+//        //clk와 상관없이 q값이 0이 됨 reset우선
+//        if(reset_p) q = 0;
+//        else if(en)q <= d;
+//    end
+
+    always @(posedge clk)begin
+        //clk에 맞춰 리셋됨
+        if(reset_p) q = 0;
+        else if(en)q <= d;
+    end
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
